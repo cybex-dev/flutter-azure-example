e06009e35e9539bdf142941ef067ea9c03283232
Charles Dyason <k4gcybex@gmail.com>
2023-07-30 18:45:11 +0200

    Replace public directory in app.js

--- app.js
+++ app.js
@@ -14,8 +14,7 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'build/web')));
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
